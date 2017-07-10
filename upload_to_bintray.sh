56933f4937801904ad10f2858f302b80c7680c30
Bruce Sun <bruce.sun@tubemogul.com>
2017-07-10 16:18:38 +0800

    Build 3.5.3.

--- upload_to_bintray.sh
+++ upload_to_bintray.sh
@@ -2,9 +2,6 @@
 
 echo "*****************Start upload_to_bintray.sh*****************"
 
-echo "Output of 'ls -alh $PYTHON_TARBALL_DIR':"
-ls -alh $PYTHON_TARBALL_DIR
-
 if ! [ -z ${PYTHON_2_VERSION+x} ]; then
     echo "Uploading python2 binary tarball to bintray..."
     curl -v -u $BINTRAY_USER_NAME:$BINTRAY_API_KEY --upload-file $TRAVIS_BUILD_DIR/dist/$PYTHON_2_TARBALL_SAVE_NAME "https://api.bintray.com/content/athrunsun/pyenv_python_build/python2/stable/$PYTHON_2_TARBALL_SAVE_NAME?publish=1&override=1"
